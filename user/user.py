b7dc15a5501b91b032c36d2d5c45ee0de317fc04
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-09-23 00:36:03 +0200

    make basic api for user

--- user/user.py
+++ user/user.py
@@ -11,10 +11,44 @@ HOST = '0.0.0.0'
 with open('{}/databases/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
 
+def write(users):
+    with open('{}/databases/users.json'.format("."), 'w') as f:
+        full = {}
+        full['users']=users
+        json.dump(full, f)
+
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
+@app.route("/users/json", methods=['GET'])
+def get_json():
+    res = make_response(jsonify(users), 200)
+    return res
+
+@app.route("/users/<userid>", methods=['POST'])
+def add_user_to_movie(userid):
+    req = request.get_json()
+
+    for user in users:
+        if str(user["id"]) == str(userid):
+            return make_response(jsonify({"error":"user ID already exists"}),500)
+
+    users.append(req)
+    write(users)
+    res = make_response(jsonify({"message":"user added"}),200)
+    return res
+
+@app.route("/users/<userid>", methods=['DELETE'])
+def del_user(userid):
+    for user in users:
+        if str(user["id"]) == str(userid):
+            users.remove(user)
+            write(users)
+            return make_response(jsonify(user),200)
+
+    res = make_response(jsonify({"error":"user ID not found"}),500)
+    return res
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
