f7c279ea2c2b0224197f246364cb77a691b54139
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-09-23 14:11:32 +0200

    adavance on booking

--- booking/booking.py
+++ booking/booking.py
@@ -11,6 +11,14 @@ HOST = '0.0.0.0'
 with open('{}/databases/bookings.json'.format("."), "r") as jsf:
    bookings = json.load(jsf)["bookings"]
 
+with open('{}./movie/databases/movies.json'.format("."), "r") as jsf:
+   movies = json.load(jsf)["movies"]
+   print(movies)
+
+with open('{}./schedule/databases/times.json'.format("."), "r") as jsf:
+   schedule = json.load(jsf)["schedule"]
+   print(schedule)
+
 def write(bookings):
     with open('{}/databases/bookings.json'.format("."), 'w') as f:
         full = {}
@@ -26,14 +34,58 @@ def get_json():
     res = make_response(jsonify(bookings), 200)
     return res
 
+# get booking by id
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_by_id(userid):
+    for booking in bookings:
+        if str(booking["userid"]) == str(userid):
+            res = make_response(jsonify(booking),200)
+            return res
+    return make_response(jsonify({"error":"Booking ID not found"}),500)
+
+# get schedules with movies from booking by user_id
+@app.route("/bookings/<userid>/schedules", methods=['GET'])
+def get_schedules_by_userid(userid):
+   for booking in bookings:
+      if str(booking["userid"]) == str(userid):
+         result = {
+            "userid": booking["userid"],
+            "dates": []
+         }
+         for booking_date in booking["dates"]:
+            date_result = {
+               "date": booking_date["date"],
+               "movies": []
+            }
+            for movie_id in booking_date["movies"]:
+               for movie in movies:
+                  if str(movie["id"]) == str(movie_id):
+                        date_result["movies"].append(movie)
+                        break
+            result["dates"].append(date_result)
+         return make_response(jsonify(result), 200)
+
+   return make_response(jsonify({"error": "User booking not found"}), 404)
+
 # Post a new booking
 @app.route("/bookings/<userid>", methods=['POST'])
 def add_booking(userid):
     req = request.get_json()
 
+    dates = req.get("dates")
+    for booking_schedule in dates:
+      date = booking_schedule.get("date")
+      movies = booking_schedule.get("movies")
+      for movie_id in movies:
+         for scheduleItem in schedule:
+            if str(movie_id) in str(scheduleItem["movies"]) and str(scheduleItem["date"]) == str(date):
+               break
+         else:
+            return make_response(jsonify({"error":"la date réservée pour le film n'est pas correcte"}),500)
+
     for booking in bookings:
         if str(booking["userid"]) == str(userid):
-            return make_response(jsonify({"error":"booking ID already exists"}),500)
+            return make_response(jsonify({"error":"user ID already exists"}),500)
 
     bookings.append(req)
     write(bookings)
